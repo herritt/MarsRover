d4848baba0ebac374ee80749282809f31da8b89c
Randy Herritt <randy@Randys-MacBook-Pro.local>
2015-11-29 20:38:39 -0400

    Added suspision meter and a bucket with shovel.

---
+++ Assets/Scripts/Visibile.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Visibile : MonoBehaviour {
+
+	private bool isVisible = false;
+	private MeshRenderer r;
+
+	// Use this for initialization
+	void Start () {
+		r = GetComponent<MeshRenderer> ();
+		r.enabled = false;
+	}
+	
+	public void MakeActive() {
+		r.enabled = true;
+	}
+}
